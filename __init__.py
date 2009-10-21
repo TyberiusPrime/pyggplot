93997268c005c4b7890ef5b177c4338c6ed93e9b
finkernagel@finkernagel-desktop <finkernagel@finkernagel-desktop>
2009-10-21 14:06:42 +0200

    introduced object based plot model

--- __init__.py
+++ __init__.py
@@ -1,4 +1,106 @@
+import exptools
 import rpy2.robjects as robjects
+robjects.r('library(ggplot2)')
+
+class Plot:
+
+    def __init__(self, dataframe, xaxis='X', yaxis='Y'):
+        self.r = {}
+        self.r['ggplot'] = robjects.r['ggplot']
+        self.r['aes'] = robjects.r['aes']
+        self.r['add'] = robjects.r('ggplot2:::"+.ggplot"')
+        self.r['layer'] = robjects.r['layer']
+        self.r['facet_wrap'] = robjects.r['facet_wrap']
+        self.r['geom_text'] = robjects.r['geom_text']
+        self.r['ggsave'] = robjects.r['ggsave']
+        self.dataframe = dataframe
+        self._aesthetics = {} 
+        self._aesthetics['x'] = xaxis
+        self._aesthetics['y'] = yaxis
+        self._other_adds = []
+
+    def render(self, output_filename, width=8, height=6):
+        aes_params = []
+        for aes_name, aes_column in self._aesthetics.items():
+            aes_params.append('%s=%s' % (aes_name, aes_column))
+        aes_params = ", ".join(aes_params)
+        plot = self.r['ggplot'](self.dataframe, robjects.r('aes(%s)' % (aes_params,)))
+        for obj in self._other_adds:
+            plot = self.r['add'](plot, obj)
+        #plot = self.r['add'](plot, self.r['layer'](geom="point"))
+        #robjects.r('options( error=recover )')
+        self.r['ggsave'](filename=output_filename,plot=plot, width=width, height=height)
+
+    def add_aesthetic(self, name, column_name):
+        self._aesthetics[name] = column_name
+
+    def add_scatter(self, x_column, y_column):
+        self._other_adds.append(self.r['layer'](geom="point"))
+        self.add_aesthetic('x',x_column)
+        self.add_aesthetic('y',y_column)
+
+    def _build_aesthetic(self, params):
+        aes_params = []
+        for aes_name, aes_column in params.items():
+            aes_params.append('%s=%s' % (aes_name, aes_column))
+        aes_params = ", ".join(aes_params)
+        return robjects.r('aes(%s)' % aes_params)
+
+
+    def add_line(self, x_column, y_column, color=None):
+        aes_params = {'x': x_column, 'y': y_column}
+        if color:
+            aes_params['colour'] = color
+        print self._build_aesthetic(aes_params)
+        self._other_adds.append(robjects.r('geom_line')(self._build_aesthetic(aes_params)))
+        #self.add_aesthetic('x',x_column)
+        ###self.add_aesthetic('y',y_column)
+
+    def set_title(self, title):
+        self._other_adds.append(robjects.r('opts(title = "%s")' %  title))
+
+    def add_vertical_bar(self, xpos, alpha=0.5):
+        self._other_adds.append(
+            robjects.r('geom_vline(aes(xintercept = %i),  alpha=%f)' % (xpos, alpha))
+        )
+
+    def add_segment(self, xstart, xend, ystart, yend, color, alpha = 1):
+        self._other_adds.append(
+            robjects.r('geom_segment')
+            (
+                robjects.r('aes(x=x, y=y, xend=xend, yend=yend)'),
+                exptools.DataFrame.DataFrame({"x": [xstart], 'xend': [xend], 'y': [ystart], 'yend': [yend]}),
+                colour=color,
+                alpha = alpha
+
+            )
+        )
+
+
+    def greyscale(self):
+        self._other_adds.append( robjects.r('scale_colour_grey()'))
+        self._other_adds.append( robjects.r('scale_fill_grey()'))
+
+    def theme_bw(self):
+        self._other_adds.append(robjects.r('theme_bw()'))
+        
+    def add_text(self, text, xpos, ypos):
+        import exptools
+        data = exptools.DataFrame.DataFrame({'x': [xpos], 'y': [ypos], 'text': [text]})
+        self._other_adds.append(
+            self.r['geom_text'](
+               robjects.r('aes(x=x, y=y, label=text)'),
+               data,
+                size=3,
+                color="black"
+
+            )
+        )
+
+
+
+        
+
 
 def doGGBarPlot(dataframe,title, xaxis, yaxis, color, facet, output_filename):
     robjects.r('library(ggplot2)')
@@ -83,9 +185,37 @@ def doScatterPlot(dataframe, title, xaxis, yaxis, color=None, output_filename = 
         plot = add(plot, robjects.r('scale_colour_gradient()'))
     #plot = add(plot, robjects.r('scale_fill_brewer("Set1")'))
     #plot = add(plot, robjects.r('opts(title = "%s")' %  title))
-    print dataframe
     ggsave(filename=output_filename,plot=plot, width=8, height=6)
 
+def plotArray(numpy_array, title, xaxis_name = 'x', yaxis_name = 'y', xaxis_offset = 0, color=None,  output_filename = False):
+    if not output_filename:
+        raise ValueError("You have to specify an output_filename")
+    xs = []
+    ys = []
+    for (x, y) in enumerate(numpy_array):
+        xs.append(x + xaxis_offset)
+        ys.append(y)
+    import exptools
+    df = exptools.DataFrame.DataFrame({xaxis_name: xs, yaxis_name: ys})
+    robjects.r('library(ggplot2)')
+    ggplot = robjects.r['ggplot']
+    aes = robjects.r['aes']
+    add = robjects.r('ggplot2:::"+.ggplot"')
+    layer = robjects.r['layer']
+    facet_wrap = robjects.r['facet_wrap']
+    ggsave = robjects.r['ggsave']
+
+    aes_params = []
+    aes_params.append('x=%s' % xaxis_name)
+    aes_params.append('y=%s' % yaxis_name)
+    if color:
+        aes_params.append('color=%s'  % color)
+    aes_params = ", ".join(aes_params)
+    plot = ggplot(dataframe, robjects.r('aes(%s)' % (aes_params, )))
+    plot = add(plot, layer(geom="point"))
+    if color:
+        plot = add(plot, robjects.r('scale_colour_gradient()'))
+    ggsave(filename=output_filename)
 
 def doJitterPlot(dataframe, title, xaxis, yaxis, group=None, color=None, size=None,shape=None, output_filename = False):
     if not output_filename:
