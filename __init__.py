466e069de4e8b5707345e0796d695b25838cc892
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2011-03-29 11:37:59 +0200

    improved pie histogram chart helper

--- __init__.py
+++ __init__.py
@@ -170,12 +170,14 @@ class Plot:
         if stat_params:
             other_params['stat'] = robjects.r('stat_bin')(**stat_params)
             other_params['position'] = position
+            print 'a', other_params
             self._other_adds.append(
                 robjects.r('geom_bar')(self._build_aesthetic(aes_params), 
                                    **other_params)
             )
         else:
             other_params['position'] = position
+            print 'b', other_params
             self._other_adds.append(
                 robjects.r('geom_bar')(self._build_aesthetic(aes_params), **other_params)
             )
@@ -909,6 +911,7 @@ class Plot:
 
     def coord_flip(self):
         self._other_adds.append(robjects.r('coord_flip()'))
+        return self
 
     def coord_polar(self, theta="x", start=0, direction=1, expand=False):
         self._other_adds.append(robjects.r('coord_polar')(
@@ -916,6 +919,7 @@ class Plot:
             start = start,
             direction = direction,
             expand = expand))
+        return self
 
 
     def legend_position(self, value):
@@ -1111,9 +1115,10 @@ def PlotPiechartHistogram(df, column_name, include_counts = True, include_percen
         current += cc
     total = current
     labels = []
-    for cc in counts:
+    label_positions = []
+    for ii, cc in enumerate(counts):
         if include_counts and include_percentage:
-            l = "%i\n(%.2f%%)" % (cc, float(cc) / total * 100)
+            l = "%i (%.2f%%)" % (cc, float(cc) / total * 100)
         elif include_counts:
             l = "%i" % cc
         elif include_percentage:
@@ -1121,6 +1126,7 @@ def PlotPiechartHistogram(df, column_name, include_counts = True, include_percen
         else:
             l = ""
         labels.append(l)
+        label_positions.append([2, 2.25, 2.5][ii % 3])
 
 
     count_column = 'count'
@@ -1131,12 +1137,14 @@ def PlotPiechartHistogram(df, column_name, include_counts = True, include_percen
         'Counts': counts,
         'Values': [str(x) for x in ordered],
         'Labels': labels,
+        'Label_positions': label_positions,
         'y': positions
     })
     plot = Plot(plot_df, 'Dummy')
     plot.add_bar_plot("Dummy", 'Counts', fill="Values", position="stack")
     if include_percentage or include_counts:
-        plot.add_text('Dummy','y', 'Labels')
+        plot.add_text('Label_positions','y', 'Labels', color="Values")
+        #plot.add_segment(0, 'Label_positions', 1, 'y', color="Values")
     plot.coord_polar(theta='y')
     plot.hide_x_axis_labels()
     plot.hide_x_axis_title()
