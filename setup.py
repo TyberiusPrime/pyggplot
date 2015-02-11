2aac8f816da9bebd3074f91cf48d1479dcd0d1ff
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2015-02-11 11:23:24 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='pyggplot',
+    version='1',
+    packages=['pyggplot',],
+    license='BSD',
+    #url='http://code.google.com/p/pydataframe/',
+    author='Florian Finkernagel',
+    description = "A pythonic wrapper around R's ggplot",
+    author_email='finkernagel@coonabibba.de',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'pandas>=0.15',
+        'rpy2',
+        ]
+)
