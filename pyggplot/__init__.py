caaa3a24b6b8c716554452f8d9cd8298807e99a5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2015-11-06 13:55:53 +0100

    introduced scale_fill_many_categories

--- pyggplot/__init__.py
+++ pyggplot/__init__.py
@@ -1013,6 +1013,20 @@ class Plot:
         self._other_adds.append(robjects.r('theme')(**{"axis.title.x": robjects.r('element_blank()')}))
         return self
 
+    def scale_fill_many_categories(self):
+        self.scale_fill_manual(["dodgerblue2","#E31A1C", # red
+                "green4",
+                "#6A3D9A", # purple
+                "#FF7F00", # orange
+                "black","gold1",
+                "skyblue2","#FB9A99", # lt pink
+                "palegreen2",
+                "#CAB2D6", # lt purple
+                "#FDBF6F", # lt orange
+                "gray70", "khaki2",
+                "maroon","orchid1","deeppink1","blue1","steelblue4",
+                "darkturquoise","green1","yellow4","yellow3",
+                "darkorange4","brown"])
     def scale_fill_manual(self, list_of_colors, guide = None):
         kwargs = {}
         if guide is not None: 
